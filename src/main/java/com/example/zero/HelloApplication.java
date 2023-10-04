304de7a3257d768a16cd38f7e2d4a123d4c66cd7
daniilmozzhukhin <formulamgo2@gmail.com>
2023-10-04 16:48:03 +0300

    color

--- src/main/java/com/example/zero/HelloApplication.java
+++ src/main/java/com/example/zero/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.scene.Camera;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.stage.Stage;
 
@@ -15,12 +16,15 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         Box box = new Box(200, 20, 50);
+        PhongMaterial material = new PhongMaterial(Color.ROYALBLUE);
+        box.setMaterial(material);
+
         GroupExtended group = new GroupExtended();
         group.getChildren().add(box);
 
         Camera camera = new PerspectiveCamera(true);
         Scene scene = new Scene(group, 1440, 800);
-        scene.setFill(Color.SILVER);
+        scene.setFill(Color.LIGHTPINK);
         scene.setCamera(camera);
         camera.translateXProperty().set(0);
         camera.translateYProperty().set(0);
